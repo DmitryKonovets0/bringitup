6312241917bfb1abdd65906ddf5dcc19aa8b0d5d
DmitryKonovets0 <dmitrykonovets@gmail.com>
2024-01-17 19:36:24 +0200

    include functionality for appearances block with teacher

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -1,44 +1,45 @@
 export default class Slider {
-    constructor(page, btns){
+    constructor(page, btn) {
         this.page = document.querySelector(page);
         this.slides = Array.from(this.page.children);
-        this.btns = document.querySelectorAll(btns);
+        this.btn = document.querySelectorAll(btn);
         this.slideIndex = 1;
     }
-
     showSlides(n) {
-        if (n > this.slides.length) {
-            this.slideIndex = 1;
+        if(n < 1) {
+            this.slideIndex = this.slides.length
         }
-
-        if (n < 1) {
-            this.slideIndex = this.slides.length;
+        if(n > this.slides.length) {
+            this.slideIndex = 1
         }
-
         this.slides.forEach(slide => {
-            slide.style.display = 'none';
-        });
-
-        this.slides[this.slideIndex - 1].style.display = 'block';
+            slide.style.display = 'none'
+        })
+        this.slides[this.slideIndex - 1].style.display = 'block'
     }
-
-    plusSlides(n) {
+    plusSlide(n) {
         this.showSlides(this.slideIndex += n);
     }
-
     render() {
-        this.btns.forEach(item => {
-            item.addEventListener('click', () => {
-                this.plusSlides(1);
-            });
+        this.hanson = document.querySelector('.hanson')
+        this.hanson.classList.add('animated', 'fadeInUp')
 
-            item.parentNode.previousElementSibling.addEventListener('click', (e) => {
-                e.preventDefault();
-                this.slideIndex = 1;
-                this.showSlides(this.slideIndex);
-            });
-        });
+        this.btn.forEach(btn => {
+            btn.addEventListener('click', () => {
+                console.log(this.slideIndex)
+                this.plusSlide(1)
+                if(this.slideIndex == 2) {
+                    setTimeout(() => {
+                        this.hanson.style.display = 'block'
+                    }, 3000)
+                }
+            })
+            btn.parentNode.previousElementSibling.addEventListener('click', (e) => {
+                e.preventDefault()
+                this.slideIndex = 1
+                this.showSlides(this.slideIndex)
+            })
+        })
 
-        this.showSlides(this.slideIndex);
     }
 }
