be93b4899a3f8014c2bd06f3e6729110e53bc044
DmitryKonovets0 <dmitrykonovets@gmail.com>
2024-01-19 21:01:22 +0200

    Implement proto for sliders

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
-import Slider from './modules/slider';
+import MainSlider from './modules/sliders/sliderMain';
 import VideoPlayer from './modules/playVideo';
 
 window.addEventListener('DOMContentLoaded', () => {
-    const slider = new Slider('.page', '.next');
+    const slider = new MainSlider({btns: '.next', page: '.page'});
     slider.render();
 
     const player = new VideoPlayer('.showup .play', '.overlay');
